a5e97bb0dda6cf96a7396cf7ab70e4600e2900a8
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-08-28 01:25:06 -0500

    Practiced using ngFor

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,9 +6,38 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   //chores = ['Complete LaunchCode prep work', 'Clean bathroom', 'Wash car'];
+   //todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
 
+   /*
+   choreLists = [["Yesterday's Chores", ['Empty dishwasher',
+      'Start LaunchCode prep work',
+      'Buy groceries']],
+   ["Today's Chores", ['Load dishwasher',
+      'Finish LaunchCode prep work',
+      'Buy groceries you forgot']],
+   ["Tomorrow's Chores", ['Empty dishwasher AGAIN',
+      'Play with LaunchCode practice code',
+      'Buy groceries AGAIN']]];
+   */
+
+   chores = [
+      {
+         title: "Yesterday's Chores", tasks: ['Empty dishwasher',
+            'Start LaunchCode prep work',
+            'Buy groceries']
+      },
+      {
+         title: "Today's Chores", tasks: ['Load dishwasher',
+            'Finish LaunchCode prep work',
+            'Buy groceries you forgot']
+      },
+      {
+         title: "Tomorrow's Chores", tasks: ['Empty dishwasher AGAIN',
+            'Play with LaunchCode practice code',
+            'Buy groceries AGAIN']
+      }
+   ];
    constructor() { }
 
    ngOnInit() {
